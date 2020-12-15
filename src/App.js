b542fcf24eafec4018dd154b5b216737eccaab7b
kim-chae-young <69830179+kim-chae-young@users.noreply.github.com>
2020-12-15 16:53:47 +0900

    2020.12.15

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Main from './routes/Main.js';
+import About from './routes/About.js';
+import Reference from './routes/Reference.js';
+import Script from './routes/Script.js';
+import Ani from './routes/Ani.js';
+import Site from './routes/Site.js';
+import Contact from './routes/Contact.js';
+import Detail from './routes/Detail.js';
+import { HashRouter, Route } from 'react-router-dom';
+import './scss/style.scss'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <Route path="/" exact={true} component={Main} />
+      <Route path="/about" component={About} />
+      <Route path="/reference" component={Reference} />
+      <Route path="/reference-detail" component={Detail} />
+      <Route path="/script" component={Script} />
+      <Route path="/ani" component={Ani} />
+      <Route path="/site" component={Site} />
+      <Route path="/contact" component={Contact} />
+    </HashRouter>
+  )
 }
-
 export default App;
